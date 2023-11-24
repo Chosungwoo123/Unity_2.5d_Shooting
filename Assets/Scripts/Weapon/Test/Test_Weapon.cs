b5b3b31d3114fb1c23c939afcee0694f6e4fa5d3
Chosungwoo123 <ejyoo12345@naver.com>
2023-11-24 20:52:39 +0900

    Update Test Shot Logic.

--- Assets/Scripts/Weapon/Test/Test_Weapon.cs
+++ Assets/Scripts/Weapon/Test/Test_Weapon.cs
@@ -20,17 +20,32 @@ public class Test_Weapon : MonoBehaviour
     
     private void Shot()
     {
+        
         Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-
+        
         RaycastHit hit;
 
         Vector3 dir = Vector3.zero;
-
+        
+        // 마우스 위치에 적이 있으면 총알 방향을 그쪽으로 정함
         if (Physics.Raycast(ray, out hit, Mathf.Infinity, enemyLayer))
         {
             dir = hit.point - shotPos.position;
-
+       
             dir = new Vector3(dir.x, 0, dir.z);
+        }
+
+        // 마우스 위치에 적이 없으면 마우스 위치로 방향을 정함
+        if (dir == Vector3.zero)
+        {
+            Plane shotPosPlane = new Plane(Vector3.up, shotPos.position);
+            float hitDist = 0.0f;
+            
+            if (shotPosPlane.Raycast(ray, out hitDist))
+            {
+                dir = ray.GetPoint(hitDist) - shotPos.position;
+                dir.y = 0;
+            }
         }
         
         var _bullet = Instantiate(bullet, shotPos.position, Quaternion.identity);
